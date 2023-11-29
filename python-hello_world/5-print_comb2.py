d7ec5a43b4844be711c6079587e412af266e533f
tsion mesfin <sionmesfin@gmail.com>
2023-11-29 23:36:11 +0300

    adding new file

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range (0, 100):
-    if i < 10:
-        print("{}{}".format(0, i), end=", ")
-    elif i == 99:
+    if i == 99:
         print(i)
     else:
-        print("{}".format(i), end=", ")
+        print("{:02}".format(i), end=", ")
