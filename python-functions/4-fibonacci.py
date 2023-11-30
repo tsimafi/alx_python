2c222335f6e98c5fc82abfd561cd0c77177eaa5f
tsion mesfin <sionmesfin@gmail.com>
2023-11-30 23:14:19 +0300

    new file

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-from 4-fibonacci import fibonacci_sequence
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
 
-print(fibonacci_sequence(6))
-print(fibonacci_sequence(1))
-print(fibonacci_sequence(0))
-print(fibonacci_sequence(20))
+    fibonacci_numbers = [0, 1]
+
+    while len(fibonacci_numbers) < n:
+        next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+        fibonacci_numbers.append(next_number)
+
+    return fibonacci_numbers[:n]
+
+# Test cases
+if __name__ == "__main__":
