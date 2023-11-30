6c53c9c2e4f068929c6151ea76920429d1810418
tsion mesfin <sionmesfin@gmail.com>
2023-11-30 22:35:28 +0300

    new file

---
+++ python-functions/2-temperature.py
@@ -0,0 +1,6 @@
+# File: 2-temperature.py
+
+def convert_to_celsius(fahrenheit):
+    # Formula to convert Fahrenheit to Celsius: (F - 32) * 5/9
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
