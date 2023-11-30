efb72eaa821f45fde5ad6bcd5b3e0b57fb8fa68c
tsion mesfin <sionmesfin@gmail.com>
2023-11-30 23:05:03 +0300

    new file

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,13 +1,7 @@
-def fibonacci_sequence(n):
-    if n <= 0:
-        return []
+#!/usr/bin/env python3
+from 4-fibonacci import fibonacci_sequence
 
-    # Initialize the list with the first n Fibonacci numbers
-    sequence = [0, 1]
-
-    # Generate the Fibonacci sequence up to the nth number
-    while len(sequence) < n:
-        next_number = sequence[-1] + sequence[-2]
-        sequence.append(next_number)
-
-    return sequence
+print(fibonacci_sequence(6))
+print(fibonacci_sequence(1))
+print(fibonacci_sequence(0))
+print(fibonacci_sequence(20))
