8992ec8b0b45f0ee432372c3de7b3ded1615813b
tsion mesfin <sionmesfin@gmail.com>
2023-11-30 22:39:27 +0300

    new file

---
+++ python-functions/6-password.py
@@ -0,0 +1,23 @@
+def validate_password(password):
+    # Check length
+    if len(password) < 8:
+        return False
+
+    # Check for at least one uppercase letter
+    if not any(char.isupper() for char in password):
+        return False
+
+    # Check for at least one lowercase letter
+    if not any(char.islower() for char in password):
+        return False
+
+    # Check for at least one digit
+    if not any(char.isdigit() for char in password):
+        return False
+
+    # Check for spaces
+    if ' ' in password:
+        return False
+
+    # If all checks pass, return True
+    return True
