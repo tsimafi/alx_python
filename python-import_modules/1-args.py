11f7833f253b30bd9d7f839489a69d837f511a72
tsion mesfin <sionmesfin@gmail.com>
2023-12-07 23:55:38 +0300

    new file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,6 +8,7 @@ if __name__=="__main__":
                 print("1 argument:")
                 print("1: {}".format(argv[1]))
         else:
-                print("{}: arguments:".format(len(argv)-1))
-        for i in range(1, len(argv)):
-           print("{}: {}".format (i, argv[i]))
+                print("{}: arguments:".format(len(argv) -1))
+                for i in range(1, len(argv)):
+                 print("{}: {}".format(i, argv[i]))
+           
