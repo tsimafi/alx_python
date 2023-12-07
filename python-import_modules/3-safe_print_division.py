4b80d6d958ac1c5832586e16609d3e6ffda34f52
tsion mesfin <sionmesfin@gmail.com>
2023-12-08 00:41:22 +0300

    adding new file

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
         result = a / b
@@ -6,14 +7,3 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
         return result
-
-# Example usage:
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
