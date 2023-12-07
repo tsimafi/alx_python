e625fa4ecfd76799c73a67725df357d12deb6fcd
tsion mesfin <sionmesfin@gmail.com>
2023-12-07 23:41:32 +0300

    new file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,12 +2,12 @@
 from sys import argv
 
 if __name__=="__main__":
-        if argv == 1:
+        if len(argv) == 1:
                 print("0 arguments.")
         elif len(argv) == 2:
                 print("1 argument:")
                 print("1: {}".format(argv[1]))
         else:
-         print("{}: arguments:".format(len(argv)-1))
+                print("{}: arguments:".format(len(argv)-1))
         for i in range(1, len(argv)):
            print("{}: {}".format (i, argv[i]))
