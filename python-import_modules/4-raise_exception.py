eab0abaa30e93410f9de1516b925cb7e32966f0b
tsion mesfin <sionmesfin@gmail.com>
2023-12-08 00:00:13 +0300

    new file

---
+++ python-import_modules/4-raise_exception.py
@@ -0,0 +1,8 @@
+def raise_exception():
+    raise TypeError("This is a deliberate TypeError")
+
+# Uncomment the following lines if you want to test the function directly
+# try:
+#     raise_exception()
+# except TypeError as te:
+#     print("Exception raised")
