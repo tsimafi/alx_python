d8616cf69ed1222c925614418661fd06583c581d
tsion mesfin <sionmesfin@gmail.com>
2023-12-07 23:37:18 +0300

    new file

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,7 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
