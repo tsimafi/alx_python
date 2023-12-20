35429326c1668d2695afed733b19a81d032e306c
tsimafi <149122465+tsimafi@users.noreply.github.com>
2023-12-20 21:25:55 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -103,6 +103,29 @@ class Rectangle(Base):
         """
         return self.__width * self.__height
 
+     def update(self, *args):
+        """
+        Assigns an argument to each attribute in the following order:
+        1st argument: id attribute
+        2nd argument: width attribute
+        3rd argument: height attribute
+        4th argument: x attribute
+        5th argument: y attribute
+
+        Args:
+        - *args: Variable number of arguments to update the attributes.
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+
     def display(self):
         """
         Prints the Rectangle instance with the character '#' to stdout.
