a7d97b2404ffbfb179e345b03e8963aea0b287d7
tsimafi <149122465+tsimafi@users.noreply.github.com>
2023-12-20 21:28:34 +0300

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -103,7 +103,7 @@ class Rectangle(Base):
         """
         return self.__width * self.__height
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
         Assigns an argument to each attribute in the following order:
         1st argument: id attribute
@@ -115,16 +115,14 @@ class Rectangle(Base):
         Args:
         - *args: Variable number of arguments to update the attributes.
         """
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        elif kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
 
     def display(self):
         """
