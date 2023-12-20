679147efcceafefef2331d682512c320fece8aa7
tsimafi <149122465+tsimafi@users.noreply.github.com>
2023-12-20 20:58:38 +0300

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
+"""
+Module: my_module
+Description: This module contains the definition of the Base class.
+"""
+
 class Base:
- __nb_objects = 0
-def __init__(self, id=None):
-  if id is None:
-    __nb_objects += 1
-    self.id = __nb_objects
-  else:
-    self.id = id
+    """
+    Base class for object management.
+    
+    Attributes:
+    - __nb_objects (int): A class variable to keep track of the number of objects created.
+    - id (int): An instance variable representing the object's ID.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes a new instance of the Base class.
+        
+        Args:
+        - id (int, optional): The ID to assign to the object. If not provided, a unique ID will be generated.
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
