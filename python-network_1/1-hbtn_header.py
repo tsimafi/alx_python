6dbd6cd81064f9ebc03d58a5b4278eba83c38b01
tsion mesfin <sionmesfin@gmail.com>
2023-12-21 21:22:39 +0300

    new file

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,7 +5,7 @@ Fetches https://alu-intranet.hbtn.io/status using the requests package
 
 import requests
 
-if __name__ == "__main__":
+if __name__name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
     
