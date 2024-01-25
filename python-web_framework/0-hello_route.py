562ccf9dc2d10c9da22b7762d59546b8794ff162
tsion mesfin <sionmesfin@gmail.com>
2024-01-25 23:31:15 +0300

    new file

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -5,7 +5,7 @@ Script that starts a Flask web application.
 
 from flask import Flask
 
-app = Flask(name)
+app = Flask(_name_)
 
 
 @app.route('/', strict_slashes=False)
@@ -14,5 +14,5 @@ def hello_hbnb():
     return 'Hello HBNB!'
 
 
-if name == "main":
+if _name_ == "_main_":
     app.run(host="0.0.0.0", port=5000)
