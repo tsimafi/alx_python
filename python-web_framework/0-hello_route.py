709af739c38d773b83f6a8a5738ef838935f7bec
tsion mesfin <sionmesfin@gmail.com>
2024-01-25 22:08:35 +0300

    new file

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application.
+"""
+
 from flask import Flask
 
 app = Flask(name)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """Display 'Hello HBNB!' on the root route."""
     return 'Hello HBNB!'
 
-if name == 'main':
-    app.run(host='0.0.0.0', port=5000)
+
+if name == "main":
+    app.run(host="0.0.0.0", port=5000)
