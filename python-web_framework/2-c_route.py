07a0935d3bb767e31d604f8d4fa6355aa3a49c51
tsion mesfin <sionmesfin@gmail.com>
2024-01-26 00:03:56 +0300

    new file

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -1,20 +1,23 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
 from flask import Flask
 
-app = Flask(name)
+app = Flask(_name_)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Display “Hello HBNB!” """
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ Display “HBNB” """
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    # Replace underscores with spaces
-    result = 'C ' + text.replace('_', ' ')
-    return result
+    """ Display “C ” followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
 
-if name == 'main':
+if _name_ == '_main_':
     app.run(host='0.0.0.0', port=5000)
