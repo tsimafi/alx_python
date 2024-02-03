16dd19918e6dc0c665486b802996fa4fcad28804
tsion mesfin <sionmesfin@gmail.com>
2024-02-03 17:08:20 +0300

    file

---
+++ python-classes/4-square.py
@@ -0,0 +1,25 @@
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print("#" * self.size)
