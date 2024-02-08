fee9bc11bb11e9044355ed988649172566ca6645
tsion mesfin <sionmesfin@gmail.com>
2024-02-08 23:41:20 +0300

    new file

--- python-data_structures/0-main.py
+++ python-data_structures/0-main.py
@@ -7,8 +7,9 @@ def no_c(my_string):
 
 # Test cases
 if __name__ == "__main__":
-    print(no_c("School"))   # Output: Holberton Shool
-    print(no_c("Chicago"))            # Output: hiago
-    print(no_c("holberton school"))          # Output:  is fun!
-    print(no_c(""))
-    print(no_c("HellcCcccooccoscccss"))
+    print(no_c("School"))               # Correct output: Shool
+    print(no_c("Chicago"))              # Correct output: hiago
+    print(no_c("Holberton"))            # Correct output: Holberton
+    print(no_c("Holberton School"))     # Correct output: Holberton Shool
+    print(no_c(""))                     # Correct output: (empty string)
+    print(no_c("HellcCcccooccoscccss")) # Correct output: Hellooosss
