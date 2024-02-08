060f0d28918afc5a59d0c71d4654f3bed0b86b28
tsion mesfin <sionmesfin@gmail.com>
2024-02-08 23:27:59 +0300

    new file

--- python-data_structures/0-main.py
+++ python-data_structures/0-main.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python3
-for o-no_c import main
+def no_c(my_string):
+    new_string = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
 
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
+# Test cases
+if __name__ == "__main__":
+    print(no_c("Holberton School"))   # Output: Holberton Shool
+    print(no_c("Chicago"))            # Output: hiago
+    print(no_c("C is fun!"))           # Output:  is fun!
