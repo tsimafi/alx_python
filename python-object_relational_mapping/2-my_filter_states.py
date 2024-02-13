77f65843ed923d8e0085a2cd07af7cadd64382fa
tsion mesfin <sionmesfin@gmail.com>
2024-02-13 22:22:10 +0300

    new file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
-"""
-Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-"""
+"""Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument."""
 
 import MySQLdb
 import sys
 
-if_name_== "_main_"
-    # Get command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+if __name__ == "__main__":
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor object using cursor() method
+    # Create a cursor object
     cursor = db.cursor()
 
-    # SQL query using format with user input
-    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id".format(state_name)
+    # Construct the query using input argument
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
 
-    # Execute the SQL query
-    cursor.execute(query)
+    # Execute the query
+    cursor.execute(query, (sys.argv[4],))
 
-    # Fetch all the rows in a list of lists
-    results = cursor.fetchall()
+    # Fetch all the rows
+    rows = cursor.fetchall()
 
-    # Display the results
-    for row in results:
+    # Display results
+    for row in rows:
         print(row)
 
-    # Close the cursor and database connection
+    # Close cursor and database connection
     cursor.close()
     db.close()
