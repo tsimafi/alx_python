a2248f8703276cf572a610862ab437e8d74b80de
tsion mesfin <sionmesfin@gmail.com>
2024-02-13 22:11:36 +0300

    new file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,36 +7,32 @@ table of hbtn_0e_0_usa where name matches the argument.
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # Retrieving MySQL credentials and search parameter from command-line arguments
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    mysql_db = sys.argv[3]
+if name == "main":
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connecting to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_user, passwd=mysql_password, db=mysql_db)
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-    # Creating a cursor object using cursor() method
+    # Create a cursor object using cursor() method
     cursor = db.cursor()
 
-    # SQL query to retrieve states where the name matches the search parameter
-    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    # SQL query using format with user input
+    query = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id".format(state_name)
 
-    try:
-        # Executing the SQL command with the user input as parameter
-        cursor.execute(sql_query, (state_name,))
-        
-        # Fetch all the rows in a list of tuples
-        results = cursor.fetchall()
-        
-        # Displaying the results
-        for row in results:
-            print(row)
+    # Execute the SQL query
+    cursor.execute(query)
 
-    except Exception as e:
-        print("Error: unable to fetch data")
-        print(e)
+    # Fetch all the rows in a list of lists
+    results = cursor.fetchall()
 
-    # Closing the database connection
+    # Display the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
     db.close()
