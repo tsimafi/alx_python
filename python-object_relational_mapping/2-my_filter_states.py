4e1b068e6bee9c5301fe3083ff8d3a011928775d
tsion mesfin <sionmesfin@gmail.com>
2024-02-13 22:09:03 +0300

    new file

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
 import MySQLdb
 import sys
 
