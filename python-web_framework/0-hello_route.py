34a4718cafaee3e562e45d795917b14325a414ff
tsion mesfin <sionmesfin@gmail.com>
2024-02-15 19:56:16 +0300

    data structure

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Route definition
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
+def hello():
+    return "Hello HBNB!"
+
 
 if __name__ == '__main__':
-    # Run the Flask app
     app.run(host='0.0.0.0', port=5000)
