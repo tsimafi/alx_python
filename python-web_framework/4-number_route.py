04d35bb6e1048bb4c30608c7c07ecce66d012715
tsion mesfin <sionmesfin@gmail.com>
2024-02-15 21:09:53 +0300

    data structure

--- python-web_framework/4-number_route.py
+++ python-web_framework/4-number_route.py
@@ -1,29 +1,23 @@
 from flask import Flask
+from urllib.parse import unquote
 
+# Create a Flask application instance
 app = Flask(__name__)
 
 # Route definitions
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Define a route for the root URL.
+    Returns:
+        str: A simple greeting message.
+    """
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    return 'C {}'.format(text.replace("_", " "))
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text="is cool"):
-    return 'Python {}'.format(text.replace("_", " "))
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return '{} is a number'.format(n)
-
-if __name__ == '__main__':
-    # Run the Flask app
-    app.run(host='0.0.0.0', port=5000)
+    """
+    Define a route for '/hbnb'.
+    Returns:
+        str: A simple message indicating 'HBN
