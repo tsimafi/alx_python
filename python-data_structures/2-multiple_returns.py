cb86a69ae0b62eca6a018a0ca824676aeb2d83db
tsion mesfin <sionmesfin@gmail.com>
2024-02-15 19:51:18 +0300

    data structure

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -3,24 +3,3 @@ def multiple_returns(sentence):
         return 0, None
     else:
         return len(sentence), sentence[0]
-
-# Test cases
-sentence = "At Holberton school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
-
-sentence = "Holberton"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
-
-sentence = "Holberton is so cool"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
-
-sentence = "H"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
-
-sentence = ""
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
