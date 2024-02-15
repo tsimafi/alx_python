f232954a2bc97fa968b6391da6379af2c7532e09
tsion mesfin <sionmesfin@gmail.com>
2024-02-15 19:46:19 +0300

    data structure

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,18 +1,10 @@
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+
     for row in matrix:
         for i in range(len(row)):
             print("{:d}".format(row[i]), end="")
             if i != len(row) - 1:
                 print(" ", end="")
         print()
-
-# Test cases
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
