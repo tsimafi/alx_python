7ea3ada89bba8ca446128a86bc5dbf967faa0f27
tsion mesfin <sionmesfin@gmail.com>
2024-02-15 20:00:20 +0300

    data structure

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,16 +1,58 @@
+"""
+1-hbnb_route.py
+
+This module implements a Flask web application with two routes.
+
+The web application listens on 0.0.0.0, port 5000. It has two routes:
+- Route: /
+  Description: Displays "Hello HBNB!" when accessed.
+- Route: /hbnb
+  Description: Displays "HBNB" when accessed.
+
+Usage:
+    Run this module with Python to start the Flask web application:
+        $ python3 1-hbnb_route.py
+
+Routes:
+    - /: Display "Hello HBNB!"
+        - Uses the option strict_slashes=False in route definition.
+    - /hbnb: Display "HBNB"
+        - Uses the option strict_slashes=False in route definition.
+
+Example:
+    $ curl 0.0.0.0:5000/hbnb
+    HBNB
+
+Author:
+    [Your Name]
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
-# Route definitions
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
+def hello():
+    """
+    Defines the behavior for the route '/'.
+
+    Returns:
+        str: The string "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
+    """
+    Defines the behavior for the route '/hbnb'.
+
+    Returns:
+        str: The string "HBNB".
+    """
+    return "HBNB"
+
 
 if __name__ == '__main__':
-    # Run the Flask app
     app.run(host='0.0.0.0', port=5000)
