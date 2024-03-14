87c947864335884252fae5e10621b69e4dbfc113
tsion mesfin <sionmesfin@gmail.com>
2024-03-14 10:22:08 +0300

    new file

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,20 @@
-"class Square:"
+class Square:
+    """A class representing a square.
+
+    Attributes:
+        size (int): The size of the square's sides.
+
+    Raises:
+        TypeError: If the size is not an integer.
+        ValueError: If the size is negative.
+    """
+
     def __init__(self, size=0):
+        """Initialize a Square instance with a given size.
+
+        Args:
+            size (int, optional): The size of the square's sides. Defaults to 0.
+        """
         self.__size = size
 
         if type(size) is not int:
